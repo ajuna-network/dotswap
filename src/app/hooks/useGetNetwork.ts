d45e6e1046fc882d7865244dabb569fa8fd5526e
Nikola Grujicic <126670759+ngrujicic@users.noreply.github.com>
2024-03-01 09:55:32 +0100

    fix: added comment

--- src/app/hooks/useGetNetwork.ts
+++ src/app/hooks/useGetNetwork.ts
@@ -2,6 +2,7 @@ import { NETWORKS } from "../../networkConfig";
 import { NetworkKeys } from "../types/enum";
 
 const useGetNetwork = () => {
+  // change network in networkConfig for testing purposes
   return NETWORKS[NetworkKeys.Kusama];
 };
 
