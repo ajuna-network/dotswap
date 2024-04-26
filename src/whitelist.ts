6dbcfd9f5c5e4bc33641e5fe2c13cd0f346f48fc
rj-kivos <robert.jakupak@kivos.io>
2024-04-26 21:41:18 +0200

    fix: whitelisted tokens revert

--- src/whitelist.ts
+++ src/whitelist.ts
@@ -3,5 +3,4 @@
 
 // '' = native, 1984 = USDt, 1337 = USDC, 30 = DED || Polkadot
 // comment for Sourabh to add the id for DOTA token
-// export const whitelist = ["", "1984", "1337", "30", "23"];
-export const whitelist = ["", "1984", "1337"];
+export const whitelist = ["", "1984", "1337", "30", "23"];
